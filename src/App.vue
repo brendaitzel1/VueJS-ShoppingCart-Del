9d5539f3807d79845d41fd45785cdeffc7b254c0
Brenda Itzel <75551553+brendaitzel1@users.noreply.github.com>
2023-09-27 17:57:16 +0000

    ⭐Agregando v-for

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <script setup>
- import { ref } from 'vue'
-const header = ref('🛒 Shopping List App')
+import { ref } from 'vue';
+const header = ref('App Lista de compras');
 const items = ref([
   {id: 1, label: '10 bolillos 🥖'},
-  {id: 2, label: '1 lata de frijoles 🫘'},
+  {id: 2, label: '1 lata de frijoles🫘'},
   {id: 3, label: '2 lata de atún 🐟'}
 ]);
 </script>
 
 <template>
-<h1>{{ header }}</h1>
-<ul>
-  <li v-for="({ id, label }, index) in items" v-bind:key="id">
-    🔹 - {{ index }} - {{ label }}
-  </li>
-</ul>
+  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+  <ul>
+    <li v-for="{ id, label } in items" v-bind:key="id">
+      🔹 {{ label }}
+    </li>
+  </ul>
 </template>
-
 
 <style scoped>
 .shopping-cart-icon{
